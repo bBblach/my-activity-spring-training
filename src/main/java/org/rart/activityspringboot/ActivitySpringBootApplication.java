cf8319a2e1590311219d552855644624806f4629
Artur <artur.rapczynski@hycom.pl>
2018-10-15 12:11:48 +0200

    initial commit

---
+++ src/main/java/org/rart/activityspringboot/ActivitySpringBootApplication.java
@@ -0,0 +1,12 @@
+package org.rart.activityspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitySpringBootApplication.class, args);
+	}
+}
